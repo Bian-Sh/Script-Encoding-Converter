a8783fe09071e03549ff701dd29e6bc75fb51a41
雨落随风 <bshsf@qq.com>
2022-09-20 21:33:22 +0800

    加入license，使得出处可追溯

--- Assets/ScriptEncodingConverter/Editor/ScriptEncodingConverter.cs
+++ Assets/ScriptEncodingConverter/Editor/ScriptEncodingConverter.cs
@@ -1,3 +1,8 @@
+
+// Copyright (c) Bian Shanghai
+// https://github.com/Bian-Sh/Script-Encoding-Converter
+// Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
+
 using System;
 using System.Collections.Generic;
 using System.IO;
